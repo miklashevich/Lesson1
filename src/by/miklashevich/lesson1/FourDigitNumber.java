2003e3ebb5545617d9d7d5b135936fc6b69672cd
miklashevich <35031208+miklashevich@users.noreply.github.com>
2021-01-31 17:19:48 +0300

    add  task 5 (FourDigitNumber)

---
+++ src/by/miklashevich/lesson1/FourDigitNumber.java
@@ -0,0 +1,26 @@
+package by.miklashevich.lesson1;
+
+public class FourDigitNumber {
+    public static void main(String[] args) {
+        int abcd = 3333;
+        int d = abcd % 10;
+
+        int abc = abcd / 10;
+        int c = abc % 10;
+
+        int ab = abc / 10;
+        int b = ab % 10;
+
+        int a = ab / 10;
+
+        if (a==b & b==c & c==d) {
+            System.out.println(" все числа равны");
+        } else {
+            System.out.println(" все числа не равны");
+        }
+
+
+
+
+    }
+    }
