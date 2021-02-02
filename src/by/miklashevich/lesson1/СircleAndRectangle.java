36906e5b344740ba0fa2c138a75d03347c5a0724
miklashevich <35031208+miklashevich@users.noreply.github.com>
2021-02-02 10:17:03 +0300

    task 7 (correct version with brackets)

--- src/by/miklashevich/lesson1/СircleAndRectangle.java
+++ src/by/miklashevich/lesson1/СircleAndRectangle.java
@@ -3,15 +3,20 @@ package by.miklashevich.lesson1;
 public class СircleAndRectangle {
 
         public static void main(String[] args) {
-          int a=56;
-          int b=50;
-          int r=38;
-            if (r * r >= (a * a + b * b) / 4)
-                System.out.println("Отверстие со сторонами" +" " + a +" " + "и" +" " + b  +" " + "Можно закрыть картонкой с радиусом" + " "  +r);
-            else
-            System.out.println("Отверстие со сторонами" +" " + a +" " + "и" +" " + b  +" " + "Нельзя закрыть картонкой с радиусом" + " "  +r);
-        }
-    }
+          int a=50;
+          int b=23;
+          int r=12;
+            if (r * r >= (a * a + b * b) / 4) {
+
+                System.out.println("Отверстие со сторонами" + " " + a + " " + "и" + " " + b + " " + "Можно закрыть картонкой с радиусом" + " " + r);
+            }
+            else {
+                System.out.println("Отверстие со сторонами" + " " + a + " " + "и" + " " + b + " " + "Нельзя закрыть картонкой с радиусом" + " " + r);
+            }
+
+}
+}
+
 
 
 
