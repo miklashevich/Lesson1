ad99ed767fb725aa579fc787d6fd383764fd31bc
miklashevich <35031208+miklashevich@users.noreply.github.com>
2021-02-02 11:07:33 +0300

    add fixes

--- src/by/miklashevich/lesson1/EvenOdd.java
+++ src/by/miklashevich/lesson1/EvenOdd.java
@@ -2,22 +2,22 @@ package by.miklashevich.lesson1;
 
 public class EvenOdd {
     public static void main(String[] args) {
-        int n;
-        n = 87;
+        int n = 232;
 
-        if (n/100>=1 & n/100<=9) {
+
+        if (n / 100 >= 1 & n / 100 <= 9) {
             System.out.println(n + " Трехзначное число");
         } else {
             System.out.println(n + " - Число не трехзначное");
         }
 
-        if(n%2==0) {
+        if(n % 2 == 0) {
             System.out.println(n+" - Четное число");
         } else {
             System.out.println(n+" - Нечетное число");
         }
 
-        if(n%10==7) {
+        if(n % 10 == 7) {
             System.out.println(n+" - последнее число 7");
         } else {
             System.out.println(n+" - последнее число не 7");
--- src/by/miklashevich/lesson1/СircleAndRectangle.java
+++ src/by/miklashevich/lesson1/СircleAndRectangle.java
@@ -3,10 +3,10 @@ package by.miklashevich.lesson1;
 public class СircleAndRectangle {
 
         public static void main(String[] args) {
-          int a=50;
-          int b=23;
-          int r=12;
-            if (r * r >= (a * a + b * b) / 4) {
+          double a=50.5;
+          double b=23.3;
+          double r=12.0;
+            if (r * r >= (a * a + b * b) / 4.0) {
 
                 System.out.println("Отверстие со сторонами" + " " + a + " " + "и" + " " + b + " " + "Можно закрыть картонкой с радиусом" + " " + r);
             }
