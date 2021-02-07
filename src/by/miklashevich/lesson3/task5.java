f8ed17a9ab1777dab723ce4e7466b540440af8de
miklashevich <35031208+miklashevich@users.noreply.github.com>
2021-02-07 11:42:41 +0300

    task5

---
+++ src/by/miklashevich/lesson3/task5.java
@@ -0,0 +1,22 @@
+package by.miklashevich.lesson3;
+
+public class task5 {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int result = 1;
+    for (int i = 3; i <= 24; i = i + 3) { // цикл с шагом 3, до 24
+
+        result = result * 2; // получаем результат цикла - сколько амеб стало в цикле
+
+
+
+    }
+
+        System.out.println(result);
+
+
+
+    }
+}
